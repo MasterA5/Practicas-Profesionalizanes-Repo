6a722bc79be82bb7790e39c6929e2bf088d4567f
aagusss214 <elsupertv963@example.com>
2026-04-10 18:06:35 -0300

    adding header and else case

--- insertar.php
+++ insertar.php
@@ -15,6 +15,7 @@ $sql = "INSERT INTO alumnos (nombre_apellido, dni, direccion, fecha_de_nacimient
 if (mysqli_query($conexion, $sql)) {
     echo '<h2>¡Registro Exitoso</h2>';
     echo '<a href="index.html">Volver Al Formulario</a>';
+    header("location:lista_alu.php");
 } else {
     echo 'Error Al Insertar Datos' . mysqli_error($conexion);
 }
